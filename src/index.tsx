9c80d443be160ccfa09ee40af053ac6fb2ca32b4
Hessamoddin A Shokravi <ahs502@gmail.com>
2022-09-18 17:40:37 +0430

    Improve test scripts and test before commit

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,11 @@ root.render(
     <App />
   </React.StrictMode>
 )
+
+fetch(`https://api.thedogapi.com/v1/breeds?page=1&limit=10`, {
+  headers: {
+    'x-api-key': 'live_fhxlFssMcdVApH5CHU1N9n1y7cxnJ6NFHJdeg2361HPWN1w5oiEVOpTqBeQ7NWBg',
+  },
+})
+  .then(response => response.json())
+  .then(console.log)
