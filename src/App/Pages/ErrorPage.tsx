4a7c994693b541af325b452ae83d485b933faa0c
Hessamoddin A Shokravi <ahs502@gmail.com>
2022-09-19 19:44:10 +0430

    Improve error page styling

--- src/App/Pages/ErrorPage.tsx
+++ src/App/Pages/ErrorPage.tsx
@@ -1,18 +1,48 @@
-import { useEffect } from 'react'
+import { Button, Typography } from '@mui/material'
+import { useLayoutEffect } from 'react'
 import { isRouteErrorResponse, useNavigate, useRouteError } from 'react-router-dom'
+
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles({ name: 'ErrorPage' })((theme, _, classes) => ({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: theme.spacing(4),
+  },
+}))
 
 export function ErrorPage() {
   const routeError = useRouteError()
 
   const navigate = useNavigate()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (isRouteErrorResponse(routeError) && routeError.status === 404) {
       navigate('/', { replace: true })
+    } else {
+      console.error(routeError)
     }
   }, [routeError])
 
-  console.log(...[routeError, 'routeError ='].reverse()) //!Delete it
+  const { classes } = useStyles()
 
-  return <>ErrorPage!</>
+  return (
+    <div className={classes.root}>
+      <Typography variant="h5">Oops! Something unexpected happened.</Typography>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          navigate('/')
+        }}
+      >
+        Reset the page
+      </Button>
+    </div>
+  )
 }
